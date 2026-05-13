aa2e7dd41cc1a03962689e6097aee93c077e6f5c
vigo <tarek7salamaa@gmail.com>
2026-05-13 11:30:31 +0300

    GetSumOfNumbers Functions

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   include './string.php';
   include './array.php';
 
-  $arr = [1, 2, -3, 4, 5, -1, 12];
+  $arr = [1, 2, 3, 4];
 
-  echo MinimumNumber($arr) . '<br>';
-  echo MaximumNumber($arr);
+  echo GetSumOfNumbers($arr) . '<br>';
